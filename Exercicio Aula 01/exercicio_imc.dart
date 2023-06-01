a8cf700d0e8c967914dce54d739fbdb8fc0c8ba2
RoneyFabio <roneyfabio@gmail.com>
2023-06-01 13:44:43 -0300

    Colocado os Comentários

--- Exercicio Aula 01/exercicio_imc.dart
+++ Exercicio Aula 01/exercicio_imc.dart
@@ -1,3 +1,11 @@
+/* 
+
+O (IMC) índice de massa corporal é baseado na altura e peso do individuo.
+O calculo é representado por Dividir o peso pela Altura ao quadradro²
+
+Cria uma função que realize o calculo de IMC.
+
+*/
 main() {
   print(calculaImc(altura: 1.75, peso: 92.9));
 }
